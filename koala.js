22a8899b3a18f6cfb4c5bf621deb26ad669cd87e
MrAmericanMike <american2010@hotmail.com>
2020-10-16 20:07:15 -0300

    Koala

---
+++ koala.js
@@ -0,0 +1,22 @@
+(function (root, factory) {
+	if (typeof define === "function" && define.amd) {
+		define([], factory);
+	} else if (typeof module === "object" && module.exports) {
+		module.exports = factory();
+	} else {
+		root.returnExports = factory();
+	}
+}(typeof self !== "undefined" ? self : this, function () {
+	const K = {};
+
+	K.log = (message) => {
+		console.log(message);
+	};
+
+	return K;
+	
+}));
+
+
+
+
